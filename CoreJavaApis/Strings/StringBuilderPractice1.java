94ced72f17b5b044822c8c97ec501a73f3b8d656
stefan <istrate.stefan@protonmail.com>
2021-02-28 18:57:21 +0200

    Third Chapter

---
+++ CoreJavaApis/Strings/StringBuilderPractice1.java
@@ -0,0 +1,15 @@
+public class StringBuilderPractice1{
+	public static void main(String [] args){
+		
+		//StringBuilder is mutable
+
+		StringBuilder example1 = new StringBuilder();
+		for(char current = 'a'; current <= 'z'; current++)
+			example1.append(current);
+		System.out.println(example1);
+
+
+		//mutability and chaining
+
+	}
+}
